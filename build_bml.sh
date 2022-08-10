b1dbdde9aaea7fcca07438961cb3c2040407aa17
cnegre <christianfannegre@gmail.com>
2022-08-10 14:54:16 -0600

    Changed to default to no openmp

--- build_bml.sh
+++ build_bml.sh
@@ -6,10 +6,11 @@ MY_PATH=$(pwd)
 export CC=${CC:=gcc}
 export FC=${FC:=gfortran}
 export CXX=${CXX:=g++}
-export BLAS_VENDOR=${BLAS_VENDOR:=Generic}
+export BML_INTERNAL_BLAS=${BML_INTERNAL_BLAS:=yes}
+export BLAS_VENDOR=${BLAS_VENDOR:=None}
 export BML_OPENMP=${BML_OPENMP:=yes}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/bml/install"}
-export BML_TESTING=${BML_TESTING:=yes}
+export BML_TESTING=${BML_TESTING:=no}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
 
 cd bml; ./build.sh configure; cd build; make -j; make install cd $MY_PATH
--- build_progress.sh
+++ build_progress.sh
@@ -10,11 +10,11 @@ export PROGRESS_GRAPHLIB=${PROGRESS_GRAPHLIB:=no}
 export CC=${CC:=gcc}
 export FC=${FC:=gfortran}
 export CXX=${CXX:=g++}
-export BLAS_VENDOR=${BLAS_VENDOR:=Generic}
+export BLAS_VENDOR=${BLAS_VENDOR:=None}
 export PROGRESS_OPENMP=${PROGRESS_OPENMP:=yes}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/qmd-progress/install"}
-export PROGRESS_TESTING=${PROGRESS_TESTING:=yes}
-export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
+export PROGRESS_TESTING=${PROGRESS_TESTING:=no}
+export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=no}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
 
 cd qmd-progress; ./build.sh configure; cd build; make -j ;make install; cd $MY_PATH
