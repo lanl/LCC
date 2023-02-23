de8ff7e93473f8ce4a73ba53b0899d96ffa50882
cnegre <christianfannegre@gmail.com>
2023-02-22 17:42:04 -0700

    changed build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mkdir build
 cd build
-cmake  -DCMAKE_PREFIX_PATH="$HOME/LCC/qmd-progress/install/;$HOME/LCC/bml/install" ../src/
-make 
+MY_PATH=`pwd`
+cmake  -DCMAKE_INSTALL_BINDIR="$MY_PATH" -DCMAKE_PREFIX_PATH="$HOME/LCC/qmd-progress/install/;$HOME/LCC/bml/install" ../src/
+make install 
