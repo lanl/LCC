954bcc91e7450d8d1baf967a988c9bba91530f1c
cnegre <christianfannegre@gmail.com>
2023-03-08 19:08:13 -0700

    workflow

--- clone_libs.sh
+++ clone_libs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-rm -rf bml; git clone git@github.com:lanl/bml.git 
-rm -rf qmd-progress; git clone git@github.com:lanl/qmd-progress.git 
+rm -rf bml; git clone https://github.com/lanl/bml.git
+rm -rf qmd-progress; git clone https://github.com/lanl/qmd-progress.git 
  
